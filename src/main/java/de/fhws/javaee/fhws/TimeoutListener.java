2d5039a715adc7881db0a0142b1344a2859bfdf6
mr678 <matthias.reining@outlook.com>
2014-10-17 15:59:42 +0200

    JSP Beispiel

---
+++ src/main/java/de/fhws/javaee/fhws/TimeoutListener.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fhws.javaee.fhws;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class TimeoutListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("Session wurde angelegt " );
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("Session wurde gelöscht " );
+    }
+}
---
+++ src/main/webapp/test.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : test
+    Created on : 17.10.2014, 15:51:48
+    Author     : Matthias Reining
+--%>
+
+<%@page import="java.util.Date"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        
+        Datum: <%= new Date()%>
+    </body>
+</html>
